0bd46d6a1db94e50063dcd8e5d383a183a5fb11b
wangxun <wangxun@lvmama.com>
2018-01-09 18:29:33 +0800

    test github

---
+++ src/main/java/com/mygroup/seviceimpl/TestGit.java
@@ -0,0 +1,11 @@
+package main.java.com.mygroup.seviceimpl;
+
+/**
+ * Created by Administrator on 2018/1/9.
+ */
+public class TestGit {
+    public static void main (String args[]){
+        System.out.print("djalkj");
+    }
+
+}
